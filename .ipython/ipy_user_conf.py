9c1f8bfd0ae48786698cabff0a4af2122d9238e1
Dan Carley <dan.carley@yellgroup.com>
2011-04-08 18:08:10 +0100

    Disable auto-completion prompts and paging.

--- .ipython/ipy_user_conf.py
+++ .ipython/ipy_user_conf.py
@@ -6,13 +6,10 @@ def main():
     o = ip.options
     o.system_verbose = 0
     
-    # If you are on Linux, you may be annoyed by
-    # "Display all N possibilities? (y or n)" on tab completion,
-    # as well as the paging through "more". Uncomment the following
-    # lines to disable that behaviour
-    #import readline
-    #readline.parse_and_bind('set completion-query-items 1000')
-    #readline.parse_and_bind('set page-completions no')
+    # Disable prompt and paging for tab completion.
+    import readline
+    readline.parse_and_bind('set completion-query-items 1000')
+    readline.parse_and_bind('set page-completions no')
 
 # some config helper functions you can use 
 def import_all(modules):
