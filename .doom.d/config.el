a9c4d20b35c99212b1d3d3f0dcb2be47229fada3
Dan Carley <dan.carley@gmail.com>
2019-06-21 08:38:33 +0100

    emacs: Add projectile paths
    For the two orgs that I use most frequently, so that I can do SPC-p-p
    and partial search.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,3 +2,5 @@
 
 ;; Place your private configuration here
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+(setq projectile-project-search-path '("~/projects/go/src/github.com/dcarley/"
+                                       "~/projects/go/src/github.com/circleci/"))
