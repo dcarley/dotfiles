30cdcc28c8ecb15c70e2aa51ed3525e9f8527b39
Dan Carley <dan.carley@gmail.com>
2020-01-16 16:05:40 +0000

    lein: Multiline profiles
    So that diffs are easier to read.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,8 @@
-{:user {:dependencies [[lambdaisland/kaocha "0.0-554"]]
-        :aliases {"ktest" ["run" "-m" "kaocha.runner" "--reporter" "kaocha.report.progress/report"]}}}
+{:user {:dependencies [
+                       [lambdaisland/kaocha "0.0-554" :exclusions [org.clojure/tools.cli]]
+                       ]
+        :aliases {
+                  "ktest" ["run" "-m" "kaocha.runner" "--reporter" "kaocha.report.progress/report"]
+                  }
+        }
+ }
