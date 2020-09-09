8898e42af25962b55b4434a944248f4ff9a3f16d
Dan Carley <dan.carley@gmail.com>
2020-09-09 13:01:11 +0100

    lein: Upgrade cider-nrepl
    To match cider version after doom upgrade. Gets rid of warning when
    starting repl.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,4 @@
-{:user {:plugins [
-                  [lein-pprint "1.2.0"]
-                  [circleci/deps-plus "0.1.0-SNAPSHOT"]
-                  ]
-        }
+{:user {:plugins [[lein-pprint "1.2.0"]
+                  [circleci/deps-plus "0.1.0-SNAPSHOT"]]}
  :repl {:dependencies [[nrepl "0.6.0"]]
-        :plugins [[cider/cider-nrepl "0.24.0"]]}}
+        :plugins [[cider/cider-nrepl "0.25.3"]]}}
