84b9b8706048c6e01bdaa7099be25dc9538a1795
Dan Carley <dan.carley@gmail.com>
2020-10-18 22:45:51 +0100

    lein: Upgrade lein-pprint
    Supports `--no-pretty`.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-pprint "1.2.0"]
+{:user {:plugins [[lein-pprint "1.3.2"]
                   [circleci/deps-plus "0.1.0-SNAPSHOT"]]}
  :repl {:dependencies [[nrepl "0.6.0"]]
         :plugins [[cider/cider-nrepl "0.25.3"]]}}
