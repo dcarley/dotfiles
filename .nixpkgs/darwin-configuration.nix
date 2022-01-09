c6082b122c7b050de5c08cc1b2eed380e2131d17
Dan Carley <dan.carley@gmail.com>
2022-01-09 15:58:44 +0000

    nix: Replace brew forumulas
    Remove all things that aren't casks from Homebrew and add some of them
    to Nix system packages. I haven't reproduced everything because I
    suspect that I don't use all of them anymore and a lot I can use from
    `nix-shell` on a project basis.

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -1,15 +1,41 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages =
-    [ pkgs.vim
-    ];
+    (with pkgs; [
+      emacsMacport
+      # editor deps
+      aspell
+      ripgrep
+      git
+      fd
+      shellcheck
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+      adoptopenjdk-hotspot-bin-8
+      leiningen
+      babashka
+      # editor deps
+      clojure-lsp
+      clj-kondo
+
+      go
+      # editor deps
+      gopls
+      gocode
+      gomodifytags
+      gotests
+      gore
+
+      httpie
+      jq
+
+      tmux
+      coreutils
+      vim
+
+      pass
+      gnupg
+    ]);
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
