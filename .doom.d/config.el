71aedd0a623ab1f15ef204725d7c73799f2c7c4e
Dan Carley <dan.carley@gmail.com>
2024-09-21 22:30:10 +0100

    doom: Remove Clojure
    I haven't used this for a while. Can always reinstate if I get the urge.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -12,32 +12,6 @@
 ;; Change comments from light grey.
 (setq doom-one-brighter-comments t)
 
-;; Don't prompt for C-c C-k
-(setq cider-save-file-on-load t)
-
-;; Don't jump to warnings for C-c C-k
-(setq cider-auto-jump-to-error 'errors-only)
-
-;; https://docs.cider.mx/cider/caveats.html#_injecting_dependencies_and_leiningen_pedantic_abort_mode
-(setq cljr-suppress-middleware-warnings t)
-;; Don't need this with managed-dependencies in profiles.clj
-;; (setq cider-inject-dependencies-at-jack-in nil)
-
-;; Want to know when tests have finished
-(setq cider-test-show-report-on-success t)
-
-;; https://github.com/noctuid/lispyville#key-themes
-(setq lispyville-key-theme
-      '((operators normal)
-        c-w
-        (prettify insert)
-        (atom-movement normal visual)
-        slurp/barf-lispy
-        additional
-        additional-insert
-        ;; custom
-        additional-movement))
-
 ;; Don't go straight to file finder.
 (setq +workspaces-switch-project-function #'magit-status)
 
