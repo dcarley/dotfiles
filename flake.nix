beb5d51c3c6f97321eb5411a5615c3772711dfcd
Dan Carley <dan.carley@gmail.com>
2025-11-08 17:53:56 +0000

    nix: Update lock and Flox v1.7.6

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    flox.url = "github:flox/flox/v1.3.12";
+    flox.url = "github:flox/flox/latest";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, flox }: {
